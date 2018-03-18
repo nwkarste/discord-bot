8ec103220a46f6cec9684e9ef27085997543560b
Nathan Karsten <nwkarste@us.ibm.com>
2018-03-18 18:31:04 -0500

    fixed spells order

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function binaryIndexOf(spellBook, searchElement) {
         currentIndex = (minIndex + maxIndex) / 2 | 0;
         currentElement = spellBook[currentIndex];
         currentSpell = currentElement.name.toString().toLowerCase();
+        logger.info(currentSpell);
 
         if (currentSpell.indexOf(searchElement) === 0) {
             return currentIndex;
